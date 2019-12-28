07db869660c8eb112f76048b6b6b52cf056cf74c
elif <i.elif.balci@gmail.com>
2019-12-28 16:25:40 +0300

    refactored

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int determineLineNumber();
 void readFile();
 
 int number_of_threads;
-int maxLineLenght = 0;
+int maxLineLenght = 200;
 
 struct thread_data
 {
@@ -38,8 +38,7 @@ int main(int argc, char **args) {
     
     
     printf("File to read: %s , Thread count, respectively : %s %s %s %s \n", args[2],args[4],args[5],args[6],args[7]);
-   int lineNumber = determineLineNumber();
-   printf("line number is %d ", lineNumber);
+    int lineNumber = determineLineNumber();
     
 
 
