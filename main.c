a9c57408ef566e44040c16dd76f5f28877605b82
Anıl Reyhan <anilreyhan@gmail.com>
2019-12-29 12:53:27 +0300

    adas

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ void *readLine(void *threadid);
 int determineLineNumber();
 void readFile();
 void printLines();
+int getFileLineCount(char *fileName);
 
 
 #define MAXLINELENGTH 150
@@ -36,16 +37,17 @@ int number_of_threads;
 struct thread_data
 {
     int	thread_id;
-    int  pass;
+    int pass;
+    
 };
 
 struct line_data
 {
     char line[MAXLINELENGTH];
-    int  readFlag = 0;
-    int  upperFlag = 0;
-    int  replaceFlag = 0;
-    int  writeFlag = 0;
+    int  readFlag;
+    int  upperFlag;
+    int  replaceFlag;
+    int  writeFlag;
 };
 
 
@@ -54,37 +56,74 @@ int main(int argc, char **args) {
     
     
     
-     // To print error when usage of the program is wrong
+    // To print error when usage of the program is wrong
     if((argc != 8) || strstr(args[1], "-d") == NULL || strstr(args[3], "-n") == NULL){
         perror("ERROR: Invalid arguments");
-        perror("USAGE: ./a.out -d <directoryName> -n <#ofReadThreads> <#ofUpperThreads> <#ofReplaceThreads> <#ofWriteThreads> ");
+        perror("USAGE: ./a.out -d <fileName> -n <#ofReadThreads> <#ofUpperThreads> <#ofReplaceThreads> <#ofWriteThreads> ");
         exit(1);
     }
     
     
-    printf("File to read: %s , Thread count, respectively : %s %s %s %s \n", args[2],args[4],args[5],args[6],args[7]);
+    printf("\n\nFile to read: %s , Thread count, respectively : %s %s %s %s \n\n\n", args[2],args[4],args[5],args[6],args[7]);
     number_of_threads = atoi(args[4]);
-    readFile();
+    //readFile();
+    
+    //printLines();
+    int a = getFileLineCount(args[2]);
+    printf("A is: %d", a);
 
-    printLines();
-
-
-
+    
+    
     return 0;
 }
+
+int getFileLineCount(char *fileName){
+
+      FILE *fp;
+      int count = 0;  // Line counter (result)
+      char c;  // To store a character read from file
+    
+      // Get file name from user. The file should be
+      // either in current folder or complete path should be provided
+
+    
+      // Open the file
+      fp = fopen(fileName, "r");
+    
+      // Check if file exists
+      if (fp == NULL)
+      {
+          printf("Could not open file %s", fileName);
+          return 0;
+      }
+    
+      // Extract characters from file and store in character c
+      for (c = getc(fp); c != EOF; c = getc(fp))
+          if (c == '\n') // Increment count if this character is newline
+              count = count + 1;
+    
+      // Close the file
+      fclose(fp);
+    printf("The file %s has %d lines\n ", fileName, count);
+    return count;
+    
+}
+
+
+
 void readFile(){
     struct thread_data thread_data_array[number_of_threads];
     pthread_t threads[number_of_threads];
     int *taskids[number_of_threads];
     int rc, t;
     int pass = 2;
-
+    
     for(t=0;t<number_of_threads;t++) {
         thread_data_array[t].thread_id = t;
         thread_data_array[t].pass = pass;
         printf("Creating thread %d\n", t);
         rc = pthread_create(&threads[t], NULL, readLine, (void *)
-                &thread_data_array[t]);
+                            &thread_data_array[t]);
         if (rc) {
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
@@ -92,7 +131,7 @@ void readFile(){
     }
     pthread_join(threads[0], NULL);
     pthread_join(threads[1], NULL);
-
+    
     pthread_exit(NULL);
 }
 
@@ -100,12 +139,12 @@ void *readLine(void *threadarg){
     //Empty function for now...
     int taskid, lineNumber;
     struct thread_data *my_data;
-
+    
     my_data = (struct thread_data *) threadarg;
     taskid = my_data->thread_id;
     lineNumber = my_data->pass;
     printf("Thread id:%d, Pass=%d\n", taskid, lineNumber);
-
+    
     char buf[512];
     int count = 0;
     FILE *file = fopen("test.txt", "r");
